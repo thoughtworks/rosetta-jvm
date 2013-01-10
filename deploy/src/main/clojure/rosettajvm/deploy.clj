7f0065c4f953918dc30e248c42d5897d8ce20c04
Toby Clemson <tobyclemson@gmail.com>
2013-01-10 07:30:57 -0800

    Changing the way we run pallet which fixes the problems. Still don't know why.

--- deploy/src/main/clojure/rosettajvm/deploy.clj
+++ deploy/src/main/clojure/rosettajvm/deploy.clj
@@ -2,19 +2,15 @@
   (:require pallet.node
             pallet.core
             pallet.compute
-            pallet.configure))
-
-;(pallet.configure/defpallet
-;  :services {:aws {:provider :aws-ec2,
-;                   :identity "AKIAIDD67L7PPORJO5VA",
-;                   :credential "PbxKOyjP72x76iuySzxkwFwGDnIvidU4fltb9mv8"}})
+            pallet.configure)
+  (:gen-class rosettajvm.deploy))
 
 (defn -main [& args]
   (pallet.core/converge
     (pallet.core/group-spec "rosetta-jvm"
       :count 1
       :node-spec (pallet.core/node-spec
-                   :image {:os-family :ubuntu :image-id "us-east-1/ami-9878c0f1"}))
+                   :image {
+                            :os-family :ubuntu
+                            :image-id "us-east-1/ami-3c994355"}))
     :compute (pallet.configure/compute-service :aws)))
-
-(println (sort (ns-publics 'pallet.compute)))
