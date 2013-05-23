e9758ae3923669e22033b19e471e0bf8690583b2
Toby Clemson <tobyclemson@gmail.com>
2013-05-22 19:52:53 -0700

    Toby - Fixing upload artifacts after module rename and including deployer deb in artifacts.

--- upload-artifacts.sh
+++ upload-artifacts.sh
@@ -2,15 +2,10 @@
 
 set -e
 
-cd service/build/distributions
-ls rosetta-jvm*.tar | xargs travis-artifacts upload --target-path $TRAVIS_COMMIT --path
-
-cd -
-
-cd deployer/build/distributions
-ls deployer*.tar | xargs travis-artifacts upload --target-path $TRAVIS_COMMIT --path
-
-cd -
-
-cd package/build/distributions
+cd service-package/build/distributions
 ls rosetta-jvm*.deb | xargs travis-artifacts upload --target-path $TRAVIS_COMMIT --path
+
+cd -
+
+cd deployer-package/build/distributions
+ls deployer*.deb | xargs travis-artifacts upload --target-path $TRAVIS_COMMIT --path
